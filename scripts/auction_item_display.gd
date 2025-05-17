08245079e8a5020f031810c52c2c1dac6f6e60a9
Moosyu <70246651+Moosyu@users.noreply.github.com>
2025-05-17 19:25:01 +1200

    initial
    no code guh

--- scripts/auction_item_display.gd
+++
@@ -1,7 +0,0 @@
-extends PanelContainer
-
-## declaring labels n buttons here so theyre accessible anywhere the buttons been instantiated (and its cleaner) 
-@onready var stats_label: Label = $MarginContainer/ContentContainer/InfoContainer/StatsLabel
-@onready var fact_label: Label = $MarginContainer/ContentContainer/InfoContainer/FactLabel
-@onready var fish_name: Label = $MarginContainer/ContentContainer/TitleContainer/FishName
-@onready var icon_texture: TextureRect = $MarginContainer/ContentContainer/TitleContainer/IconTexture
--- scripts/global.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-var coins: float = 100.0
-var auction_scene = preload("res://scenes/auction.tscn")
